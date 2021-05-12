4330c4badae39bbcd8c6f096ced5324acae8085f
Ignasi  Perez <perezignasi@gmail.com>
2021-05-12 11:59:41 +0200

    SW-02-LAYOUT_BEFORE_AUTO_LAYOUT

---
+++ SW-SAMPLE_CODE/SW-02-LAYOUT_BEFORE_AUTO_LAYOUT/SW-02-02-CodeLayout-v5/CodeLayout/RootVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeLayout
+//
+//  Created by ignasiperez.com on 12/05/2021.
+//
+
+import UIKit
+
+class RootVC: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
