a8ffcec208c2108d4a241895a9f297963248c164
Ignasi  Perez <perezignasi@gmail.com>
2021-05-20 10:58:56 +0200

    SW-S0-04-USING_INTERFACE_BUILDER

---
+++ SW-SOLUTIONS/SW-S0-04-USING_INTERFACE_BUILDER/SW-SO-04-02-Challenge4-2/Challenge4-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenge4-2
+//
+//  Created by ignasiperez.com on 18/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ SW-SOLUTIONS/SW-S0-04-USING_INTERFACE_BUILDER/SW-SO-04-03-Challenge4-3/Challenge4-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenge4-3
+//
+//  Created by ignasiperez.com on 20/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
