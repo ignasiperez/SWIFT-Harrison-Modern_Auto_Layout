974a163d1328064906625c2aa1fda6482e8b52a3
Ignasi  Perez <perezignasi@gmail.com>
2021-05-25 13:04:35 +0200

    SW-SA-06-02-SafeArea-v2

---
+++ SW-SAMPLE_CODE/SW-SA-06-SAFE_AREA_AND_LAYOUT_MARGINS/SW-SA-06-02-SafeArea-v2/SafeArea-v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SafeArea-v2
+//
+//  Created by ignasiperez.com on 25/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
