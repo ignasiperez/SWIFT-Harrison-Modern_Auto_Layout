1135253049ea902f69d34bdc170820691562f443
Ignasi  Perez <perezignasi@gmail.com>
2021-05-31 18:59:25 +0200

    SW-SA-07-06-CHCR-v1_a

---
+++ SW-SAMPLE_CODE/SW-SA-07-LAYOUT_PRIORITIES_AND_CONTENT_SIZE/SW-SA-07-06-CHCR-v1_a/CHCR-v1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CHCR-v1
+//
+//  Created by ignasiperez.com on 31/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
